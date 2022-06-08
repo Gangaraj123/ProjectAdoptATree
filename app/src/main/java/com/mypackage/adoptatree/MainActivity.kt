32af28d82d11cde5787ff228fd9119847d8ce5a9
RAJU <gangarajbopparam@gmail.com>
2022-06-08 20:09:01 +0530

    added Firebase

--- app/src/main/java/com/mypackage/adoptatree/MainActivity.kt
+++ app/src/main/java/com/mypackage/adoptatree/MainActivity.kt
@@ -2,11 +2,12 @@ package com.mypackage.adoptatree
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val fb=FirebaseAuth.getInstance()
         setContentView(R.layout.activity_main)
-    }
+     }
 }
